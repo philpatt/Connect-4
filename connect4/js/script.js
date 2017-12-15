5f42d4ef2bf1e2bb03784e0ded64c3783fe25863
philpatt <phillip.s.patterson1@gmail.com>
2017-12-15 11:16:46 -0800

    mvp done, todo: draw detection, reset button, readme

--- connect4/js/script.js
+++ connect4/js/script.js
@@ -1,63 +1,62 @@
 // CONNECT 4 !
 document.addEventListener("DOMContentLoaded", function(){
 
-	addColEventListener();
+
 	var winner = document.getElementById('winner');
+	// var startRow = document.getElementById('row5');
+	addStartEventListener();
 
 })
 
 
 var col = document.getElementsByClassName('col');
-
+// var rowA = document.getElementsByClassName('rowA');
 turn = 0;
-// winner
-var addColEventListener = function(){
+
+var addStartEventListener = function(){
 	console.log('game start');
 	for(var i = 0; i < col.length; i++){
-		col[i].addEventListener('click', colClicked);
-	}
-}
-var removeColEventListener = function(){
-	console.log('game end');
-	for(var i = 0; i < col.length; i++){
-		col[i].removeEventListener('click', colClicked);
+		col[i].addEventListener('click', (colClicked));
 	}
 }
 
 var colClicked = function(){
 	turn++;
-	
 	var coords = this.id.replace('row', '').replace('col', '');
+	console.log('original coords', coords);
+	coords = fallDown(coords[0], coords[1]) + coords[1];
+	console.log('fallen coords', coords);
+	removeSingleEventListener(coords[0], coords[1]);
+	var targetElem = document.getElementById("row" + coords[0] + "col" + coords[1]);
+
 	if(turn%2 === 0){
-		this.style.backgroundColor = "black";
-		this.classList.add("blackClicked");
-		this.removeEventListener('click', colClicked);
-		console.log('black clicked');
+		targetElem.style.backgroundColor = "black";
+		targetElem.classList.add("blackClicked");
+		
+		// console.log('black clicked');
 		if(checkDiagWinner(coords[0], coords[1], 'blackClicked')){
 			winner.style.display = 'inline-block';
-			removeColEventListener();
+			removeColEventListener(coords[0], coords[1]);
 			console.log('Black WINS DIAG');
 		}
 		else if(checkDiagWinner2(coords[0], coords[1], 'blackClicked')){
 			winner.style.display = 'inline-block';
-			removeColEventListener();
+			removeColEventListener(coords[0], coords[1]);
 			console.log('Black WINS DIAG2');
 		}
 		else if(checkHorzWinner(coords[0], coords[1], 'blackClicked')){
 			winner.style.display = 'inline-block';
-			removeColEventListener();
+			removeColEventListener(coords[0], coords[1]);
 			console.log('Black WINS HORZ');
 		}
 		else if(checkVertWinner(coords[0], coords[1], 'blackClicked')){
 			winner.style.display = 'inline-block';
-			removeColEventListener();
+			removeColEventListener(coords[0], coords[1]);
 			console.log('Black WINS VERT');
 		}
-
 	} else{
-		this.style.backgroundColor = "red";
-		this.classList.add("redClicked");
-		this.removeEventListener('click', colClicked);
+		targetElem.style.backgroundColor = "red";
+		targetElem.classList.add("redClicked");
 		console.log('red clicked');
 		if (checkDiagWinner(coords[0], coords[1], 'redClicked')){
 			winner.style.display = 'inline-block';
@@ -85,13 +84,42 @@ var colClicked = function(){
 		}
 	}
 }
+
+
+
+var fallDown = function(rowNum, colNum){
+	var i = rowNum;
+	console.log('fallDown', rowNum, colNum);
+	console.log('row' + i + 'col' + colNum);
+
+
+	while (i < 6 && !document.getElementById('row' + i + 'col' + colNum).classList.contains('redClicked') &&
+		!document.getElementById('row' + i + 'col' + colNum).classList.contains('blackClicked')){
+		console.log('check clicked', i);
+		i++;		
+	}
+	console.log('i - 1', i - 1);
+	return i <= 0 ? 0 : i - 1;
+}
+
+var removeSingleEventListener = function(rowNum, colNum){
+	console.log('removing row' + rowNum + "col" + colNum + "'s event listener");
+	document.getElementById("row" + rowNum + "col" + colNum).removeEventListener('click', colClicked);
+}
+
+var removeColEventListener = function(){
+	console.log('game end');
+	for(var i = 0; i < col.length; i++){
+		col[i].removeEventListener('click', colClicked);
+	}
+}
+
 var checkVertWinner = function(rowNum, colNum, turnPlayer){
 	var count = 0;
 	var i = rowNum;
 	var j = colNum;
-
 	while (i < 6 && count < 4){
-		console.log('vertcheck loop row, col == ', i, j);
+		// console.log('vertcheck loop row, col == ', i, j);
 		if(document.getElementById('row' + i + 'col' + j).classList.contains(turnPlayer)){
 			count++;
 			i++;
@@ -100,11 +128,9 @@ var checkVertWinner = function(rowNum, colNum, turnPlayer){
 			break;
 		}
 	}
-
 	i = rowNum - 1;
-
 	while(i >= 0 && count < 4){
-		console.log('neg vertcheck loop row, col == ', i, j);
+		// console.log('neg vertcheck loop row, col == ', i, j);
 		if(document.getElementById('row' + i + 'col' + j).classList.contains(turnPlayer)){
 			count++;
 			i--;
@@ -113,7 +139,7 @@ var checkVertWinner = function(rowNum, colNum, turnPlayer){
 			break;
 		}
 	}
-	console.log('vertCount was', count);
+	// console.log('vertCount was', count);
 	if(count >= 4){
 		return true;
 	}
@@ -126,7 +152,7 @@ var checkHorzWinner = function(rowNum, colNum, turnPlayer){
 	var i = rowNum; 
 	var j = colNum;
 	while (j < 7 && count < 4){
-		console.log('vertcheck loop row, col == ', i, j);
+		// console.log('horzcheck loop row, col == ', i, j);
 		if(document.getElementById('row' + i + 'col' + j).classList.contains(turnPlayer)){
 			count++;
 			j++;
@@ -135,11 +161,9 @@ var checkHorzWinner = function(rowNum, colNum, turnPlayer){
 			break;
 		}
 	}
-
 	j = colNum - 1;
-
 	while (j >= 0 && count <4){
-		console.log('vertcheck loop row, col == ', i, j);
+		// console.log('horzcheck loop row, col == ', i, j);
 		if(document.getElementById('row' + i + 'col' + j).classList.contains(turnPlayer)){
 			count++;
 			j--;
@@ -148,7 +172,7 @@ var checkHorzWinner = function(rowNum, colNum, turnPlayer){
 			break;
 		}
 	}
-	console.log('horzCount was', count);
+	// console.log('horzCount was', count);
 	if(count >= 4){
 		return true;
 	}
@@ -162,7 +186,7 @@ var checkDiagWinner = function(rowNum, colNum, turnPlayer){
 	var j = colNum; 
 // diagonal check is down to the right
 	while (i < 6 && j < 7 && count < 4){
-		console.log('diagcheck (+)loop rowNumi, colNumj ==', i, j);
+		// console.log('diagcheck (+)loop rowNumi, colNumj ==', i, j);
 		if(document.getElementById('row' + i + 'col' + j).classList.contains(turnPlayer)){
 			count++;
 			i++;
@@ -175,12 +199,10 @@ var checkDiagWinner = function(rowNum, colNum, turnPlayer){
 // reset row and column position
 	i = Number(rowNum) - 1;
 	j = Number(colNum) - 1;
-	console.log('entering negtaive check', i, j);
-
+	// console.log('entering negtaive check', i, j);
 // diagonal check up to the left
-
 	while (i >= 0 && j >= 0 && count < 4){
-		console.log('diagcheck (-)loop rowNumi, colNumj ==', i, j);
+		// console.log('diagcheck (-)loop rowNumi, colNumj ==', i, j);
 		if(document.getElementById('row' + i + 'col' + j).classList.contains(turnPlayer)){
 			count++;
 			i--;
@@ -190,7 +212,7 @@ var checkDiagWinner = function(rowNum, colNum, turnPlayer){
 			break;
 		}
 	}
-	console.log('diagcount was', count);
+	// console.log('diagcount was', count);
 	if(count >= 4){
 		return true;
 	}
@@ -198,15 +220,13 @@ var checkDiagWinner = function(rowNum, colNum, turnPlayer){
 		return false;
 	}
 }
-
-
 var checkDiagWinner2 = function(rowNum, colNum, turnPlayer){
 	var count = 0;
 	var i = rowNum; 
 	var j = colNum; 
 	 // diagonal2 check down to the left = subtract row, add col
 	while (i >= 0  && j < 7  && count < 4){
-		console.log('diag 2 positve loop i, j ==', i, j);
+		// console.log('diag 2 positve loop i, j ==', i, j);
 		if(document.getElementById('row' + i + 'col' + j).classList.contains(turnPlayer)){
 			count++;
 			i--;
@@ -222,7 +242,7 @@ var checkDiagWinner2 = function(rowNum, colNum, turnPlayer){
 
 //  diagonal2 check down to the left = add row, sub col
 	while (i < 6 && j >= 0 && count < 4){
-		console.log('diag 2 negative loop i, j ==', i, j);
+		// console.log('diag 2 negative loop i, j ==', i, j);
 		if(document.getElementById('row' + i + 'col' + j).classList.contains(turnPlayer)){
 			count++;
 			i++;
@@ -232,7 +252,7 @@ var checkDiagWinner2 = function(rowNum, colNum, turnPlayer){
 			break;
 		}
 	}
-	console.log('diag2 count was', count);
+	// console.log('diag2 count was', count);
 	if(count >= 4){
 		return true;
 	}
@@ -240,16 +260,3 @@ var checkDiagWinner2 = function(rowNum, colNum, turnPlayer){
 		return false;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ connect4/js/script2.js
@@ -0,0 +1,59 @@
+var colClicked = function(){
+	turn++;
+	var coords = this.id.replace('row', '').replace('col', '');
+	if(turn%2 === 0){
+		this.style.backgroundColor = "black";
+		this.classList.add("blackClicked");
+		// this.removeEventListener('click', colClicked);
+		console.log('black clicked');
+		if(checkDiagWinner(coords[0], coords[1], 'blackClicked')){
+			winner.style.display = 'inline-block';
+			removeColEventListener();
+			console.log('Black WINS DIAG');
+		}
+		else if(checkDiagWinner2(coords[0], coords[1], 'blackClicked')){
+			winner.style.display = 'inline-block';
+			removeColEventListener();
+			console.log('Black WINS DIAG2');
+		}
+		else if(checkHorzWinner(coords[0], coords[1], 'blackClicked')){
+			winner.style.display = 'inline-block';
+			removeColEventListener();
+			console.log('Black WINS HORZ');
+		}
+		else if(checkVertWinner(coords[0], coords[1], 'blackClicked')){
+			winner.style.display = 'inline-block';
+			removeColEventListener();
+			console.log('Black WINS VERT');
+		}
+	} else{
+		this.style.backgroundColor = "red";
+		this.classList.add("redClicked");
+		this.removeEventListener('click', colClicked);
+		console.log('red clicked');
+		if (checkDiagWinner(coords[0], coords[1], 'redClicked')){
+			winner.style.display = 'inline-block';
+			winner.style.color = 'red'; 
+			removeColEventListener();
+			console.log('RED WINS');
+		}
+		else if (checkDiagWinner2(coords[0], coords[1], 'redClicked')){
+			winner.style.display = 'inline-block';
+			winner.style.color = 'red';
+			removeColEventListener();
+			console.log('RED WINS');
+		}
+		else if (checkHorzWinner(coords[0], coords[1], 'redClicked')){
+			winner.style.display = 'inline-block';
+			winner.style.color = 'red';
+			removeColEventListener();
+			console.log('red wins');
+		}
+		else if (checkVertWinner(coords[0], coords[1], 'redClicked')){
+			winner.style.display = 'inline-block';
+			winner.style.color = 'red';
+			removeColEventListener();
+			console.log('red wins')
+		}
+	}
+}
